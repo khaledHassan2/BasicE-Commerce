029e4610123d9c01ba97f1753a403f651a427a59
Khaled <khaledamalb22@gmail.com>
2025-09-09 20:58:49 +0300

    Add project files.

---
+++ BasicE-Commerce.Context/BasicECommerceContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicE_Commerce.Context
+{
+    public class BasicECommerceContext
+    {
+        // This class is intentionally left blank for future context-related implementations.
+    }
+}
