ff20f3c4442a4c7ee636783487b4aae0a4c66519
Ahmed <ahmedsaberkh7@gmail.com>
2025-09-10 22:07:57 +0300

    Add AllModels

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace Models
     {
         public int Quantity {  get; set; }
         public double UnitPrice {  get; set; }
+        [ForeignKey("order")]
         public int OrderID { get; set; }
         public ICollection<order > orders { get; set; }
+        [ForeignKey("Product")]
         public int ProductID { get; set; }
         public  ICollection <Product> Products { get; set; }
     }
