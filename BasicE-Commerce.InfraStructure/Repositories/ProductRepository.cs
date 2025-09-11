2f26c19d97210d23f3abe55c7eb854285fcb26d2
Mohamed Hamada <mohamedhamada97.07@gmail.com>
2025-09-11 20:06:39 +0300

    Add implementation of Repositories

---
+++ BasicE-Commerce.InfraStructure/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using BasicE_Commerce.Application.Contacts;
+using BasicE_Commerce.Context.Data;
+using BasicE_Commerce.Models;
+
+namespace BasicE_Commerce.InfraStructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product, int>, IProductRepository
+    {
+        public ProductRepository(BasicEcommerceDbContext context) : base(context)
+        {
+        }
+    }
+}
