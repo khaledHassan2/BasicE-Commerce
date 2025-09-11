8b15c02d70a06629e48d7526e469d70288b61006
Ahmed <ahmedsaberkh7@gmail.com>
2025-09-11 17:52:36 +0300

    Add DTOs

---
+++ BasicE-Commerce.DTOs/OrderDTOs/AdminOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicE_Commerce.DTOs.OrderDTOs
+{
+    public  class AdminOrder:OrderDTO
+    {
+    }
+}
---
+++ BasicE-Commerce.DTOs/UserDTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using BasicE_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicE_Commerce.DTOs.UserDTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = "user";
+
+    }
+}
