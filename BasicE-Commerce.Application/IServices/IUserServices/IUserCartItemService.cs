ee18f8bdf779b43d634aef86749e6794674027d3
alaasaber <alaasabermostafa9@gmail.com>
2025-09-12 15:50:53 +0300

    add user IService

---
+++ BasicE-Commerce.Application/IServices/IUserServices/IUserCartItemService.cs
@@ -0,0 +1,16 @@
+﻿using BasicE_Commerce.Application.Contacts;
+using BasicE_Commerce.DTOs.CartItem;
+using BasicE_Commerce.DTOs.CategoryDtos;
+using BasicE_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicE_Commerce.Application.IServices.IUserServices
+{
+    internal interface IUserCartItemService : IUserService<CartItem, int, UserCartItemDTO, CartItemCreatedDTO, ICartItemRepository>
+    {
+    }
+}
---
+++ BasicE-Commerce.Application/IServices/IUserServices/IUserCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using BasicE_Commerce.Application.Contacts;
+using BasicE_Commerce.DTOs.CategoryDtos;
+using BasicE_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicE_Commerce.Application.IServices.IUserServices
+{
+    internal interface IUserCategoryService : IUserService<Category,int,UserCategoryDTO,CategoryCreatedDTO,ICategoryRepository>
+    {
+    }
+}
