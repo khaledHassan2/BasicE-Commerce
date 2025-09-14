29bb5af12942786c5f92682c5c6444dcdd23152c
Khaled <khaledamalb22@gmail.com>
2025-09-14 09:40:19 +0300

    Design LoginForm

--- BasicE-Commerce.Presentation/IdentityForms/LoginForm.Designer.cs
+++ BasicE-Commerce.Presentation/IdentityForms/LoginForm.Designer.cs
@@ -33,63 +33,126 @@
             LoginEmailLbl = new Label();
             LoginPasswordLbl = new Label();
             LoginBtn = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // LoginEmailInput
             // 
-            LoginEmailInput.Location = new Point(259, 154);
+            LoginEmailInput.Location = new Point(470, 79);
+            LoginEmailInput.Margin = new Padding(4, 2, 4, 2);
             LoginEmailInput.Name = "LoginEmailInput";
-            LoginEmailInput.Size = new Size(265, 27);
+            LoginEmailInput.Size = new Size(291, 26);
             LoginEmailInput.TabIndex = 0;
             // 
             // LoginPasswordInput
             // 
-            LoginPasswordInput.Location = new Point(259, 232);
+            LoginPasswordInput.Location = new Point(470, 149);
+            LoginPasswordInput.Margin = new Padding(4, 2, 4, 2);
             LoginPasswordInput.Name = "LoginPasswordInput";
-            LoginPasswordInput.Size = new Size(265, 27);
+            LoginPasswordInput.Size = new Size(291, 26);
             LoginPasswordInput.TabIndex = 1;
             LoginPasswordInput.UseSystemPasswordChar = true;
             // 
             // LoginEmailLbl
             // 
             LoginEmailLbl.AutoSize = true;
-            LoginEmailLbl.Location = new Point(184, 157);
+            LoginEmailLbl.Location = new Point(470, 59);
+            LoginEmailLbl.Margin = new Padding(4, 0, 4, 0);
             LoginEmailLbl.Name = "LoginEmailLbl";
-            LoginEmailLbl.Size = new Size(46, 20);
+            LoginEmailLbl.Size = new Size(58, 18);
             LoginEmailLbl.TabIndex = 2;
             LoginEmailLbl.Text = "Email";
             // 
             // LoginPasswordLbl
             // 
             LoginPasswordLbl.AutoSize = true;
-            LoginPasswordLbl.Location = new Point(160, 235);
+            LoginPasswordLbl.Location = new Point(470, 128);
+            LoginPasswordLbl.Margin = new Padding(4, 0, 4, 0);
             LoginPasswordLbl.Name = "LoginPasswordLbl";
-            LoginPasswordLbl.Size = new Size(70, 20);
+            LoginPasswordLbl.Size = new Size(88, 18);
             LoginPasswordLbl.TabIndex = 3;
             LoginPasswordLbl.Text = "Password";
             // 
             // LoginBtn
             // 
-            LoginBtn.Location = new Point(269, 325);
+            LoginBtn.Location = new Point(470, 194);
+            LoginBtn.Margin = new Padding(4, 2, 4, 2);
             LoginBtn.Name = "LoginBtn";
-            LoginBtn.Size = new Size(216, 45);
+            LoginBtn.Size = new Size(156, 41);
             LoginBtn.TabIndex = 4;
             LoginBtn.Text = "Login";
             LoginBtn.UseVisualStyleBackColor = true;
             LoginBtn.Click += LoginBtn_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.vecteezy_3d_illustration_of_a_teenage_female_programmer_at_the_35899074;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(441, 285);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.icons8_user_48;
+            pictureBox3.Location = new Point(768, 79);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(35, 26);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.icons8_key_64;
+            pictureBox2.Location = new Point(768, 149);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(35, 26);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Courier New", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(485, 253);
+            label1.Name = "label1";
+            label1.Size = new Size(241, 22);
+            label1.TabIndex = 9;
+            label1.Text = "Don't havean account!";
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Yellow;
+            ClientSize = new Size(828, 309);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
             Controls.Add(LoginBtn);
             Controls.Add(LoginPasswordLbl);
             Controls.Add(LoginEmailLbl);
             Controls.Add(LoginPasswordInput);
             Controls.Add(LoginEmailInput);
+            Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 2, 4, 2);
             Name = "LoginForm";
             Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -101,5 +164,9 @@
         private Label LoginEmailLbl;
         private Label LoginPasswordLbl;
         private Button LoginBtn;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private Label label1;
     }
 }
--- BasicE-Commerce.Presentation/IdentityForms/RegisterForm.Designer.cs
+++ BasicE-Commerce.Presentation/IdentityForms/RegisterForm.Designer.cs
@@ -36,62 +36,71 @@
             PasswordLbl = new Label();
             SignUpBtn = new Button();
             RegisterLabel = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // RegisterNameInput
             // 
-            RegisterNameInput.Location = new Point(272, 127);
+            RegisterNameInput.Location = new Point(29, 118);
+            RegisterNameInput.Margin = new Padding(4, 2, 4, 2);
             RegisterNameInput.Name = "RegisterNameInput";
-            RegisterNameInput.Size = new Size(338, 27);
+            RegisterNameInput.Size = new Size(313, 26);
             RegisterNameInput.TabIndex = 0;
             // 
             // RegisterEmailInput
             // 
-            RegisterEmailInput.Location = new Point(272, 185);
+            RegisterEmailInput.Location = new Point(29, 171);
+            RegisterEmailInput.Margin = new Padding(4, 2, 4, 2);
             RegisterEmailInput.Name = "RegisterEmailInput";
-            RegisterEmailInput.Size = new Size(338, 27);
+            RegisterEmailInput.Size = new Size(313, 26);
             RegisterEmailInput.TabIndex = 1;
             // 
             // RegisterPasswordInput
             // 
-            RegisterPasswordInput.Location = new Point(272, 253);
+            RegisterPasswordInput.Location = new Point(29, 232);
+            RegisterPasswordInput.Margin = new Padding(4, 2, 4, 2);
             RegisterPasswordInput.Name = "RegisterPasswordInput";
-            RegisterPasswordInput.Size = new Size(338, 27);
+            RegisterPasswordInput.Size = new Size(313, 26);
             RegisterPasswordInput.TabIndex = 2;
             RegisterPasswordInput.UseSystemPasswordChar = true;
             // 
             // NameLbl
             // 
             NameLbl.AutoSize = true;
-            NameLbl.Location = new Point(177, 127);
+            NameLbl.Location = new Point(29, 98);
+            NameLbl.Margin = new Padding(4, 0, 4, 0);
             NameLbl.Name = "NameLbl";
-            NameLbl.Size = new Size(49, 20);
+            NameLbl.Size = new Size(48, 18);
             NameLbl.TabIndex = 3;
             NameLbl.Text = "Name";
             // 
             // EmailLbl
             // 
             EmailLbl.AutoSize = true;
-            EmailLbl.Location = new Point(177, 192);
+            EmailLbl.Location = new Point(29, 150);
+            EmailLbl.Margin = new Padding(4, 0, 4, 0);
             EmailLbl.Name = "EmailLbl";
-            EmailLbl.Size = new Size(46, 20);
+            EmailLbl.Size = new Size(58, 18);
             EmailLbl.TabIndex = 4;
             EmailLbl.Text = "Email";
             // 
             // PasswordLbl
             // 
             PasswordLbl.AutoSize = true;
-            PasswordLbl.Location = new Point(177, 256);
+            PasswordLbl.Location = new Point(29, 212);
+            PasswordLbl.Margin = new Padding(4, 0, 4, 0);
             PasswordLbl.Name = "PasswordLbl";
-            PasswordLbl.Size = new Size(70, 20);
+            PasswordLbl.Size = new Size(88, 18);
             PasswordLbl.TabIndex = 5;
             PasswordLbl.Text = "Password";
             // 
             // SignUpBtn
             // 
-            SignUpBtn.Location = new Point(289, 337);
+            SignUpBtn.Location = new Point(70, 280);
+            SignUpBtn.Margin = new Padding(4, 2, 4, 2);
             SignUpBtn.Name = "SignUpBtn";
-            SignUpBtn.Size = new Size(234, 29);
+            SignUpBtn.Size = new Size(229, 37);
             SignUpBtn.TabIndex = 6;
             SignUpBtn.Text = "Register";
             SignUpBtn.UseVisualStyleBackColor = true;
@@ -100,17 +109,29 @@
             // RegisterLabel
             // 
             RegisterLabel.AutoSize = true;
-            RegisterLabel.Location = new Point(340, 62);
+            RegisterLabel.Location = new Point(95, 34);
+            RegisterLabel.Margin = new Padding(4, 0, 4, 0);
             RegisterLabel.Name = "RegisterLabel";
-            RegisterLabel.Size = new Size(101, 20);
+            RegisterLabel.Size = new Size(138, 18);
             RegisterLabel.TabIndex = 7;
             RegisterLabel.Text = "Register Form";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.icons8_register_100;
+            pictureBox1.Location = new Point(369, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(372, 306);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
             // RegisterForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(753, 330);
+            Controls.Add(pictureBox1);
             Controls.Add(RegisterLabel);
             Controls.Add(SignUpBtn);
             Controls.Add(PasswordLbl);
@@ -119,8 +140,11 @@
             Controls.Add(RegisterPasswordInput);
             Controls.Add(RegisterEmailInput);
             Controls.Add(RegisterNameInput);
+            Font = new Font("Courier New", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 2, 4, 2);
             Name = "RegisterForm";
             Text = "RegisterForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -135,5 +159,6 @@
         private Label PasswordLbl;
         private Button SignUpBtn;
         private Label RegisterLabel;
+        private PictureBox pictureBox1;
     }
 }
