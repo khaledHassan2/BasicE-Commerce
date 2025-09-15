56434109f58c8886ce6d47b50033e88f0075cd5a
Mohamed Hamada <mohamedhamada97.07@gmail.com>
2025-09-15 07:13:32 +0300

    add Create Order Service

--- BasicE-Commerce.Application/Services/UserServices/UserOrderItemService.cs
+++ BasicE-Commerce.Application/Services/UserServices/UserOrderItemService.cs
@@ -2,14 +2,26 @@
 using BasicE_Commerce.Application.IServices.IUserServices;
 using BasicE_Commerce.DTOs.OrderItemDTOs;
 using BasicE_Commerce.Models;
+using Mapster;
 
 namespace BasicE_Commerce.Application.Services.UserServices
 {
     public class UserOrderItemService : UserService<OrderItem, int, UserOrderItemDTO, OrderItemCreatedDTO, IOrderItemRepository>,
         IUserOrderItemService
     {
-        public UserOrderItemService(IUnitOfWork unitOfWork, IGenericRepository<OrderItem, int> repository) : base(unitOfWork, repository)
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IOrderItemRepository _repository;
+        public UserOrderItemService(IUnitOfWork unitOfWork, IOrderItemRepository repository) : base(unitOfWork, repository)
         {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateOrderItem(OrderItemCreatedDTO orderItemCreatedDTO)
+        {
+            var orderItem = orderItemCreatedDTO.Adapt<OrderItem>();
+            _repository.Create(orderItem);
+            _unitOfWork.Commit();
         }
     }
 }
